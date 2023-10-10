6386ce624a8985de7b62c167c83ee75ea2a947ac
Bahman Timas <bahmantimas@gmail.com>
2023-10-10 18:46:35 +0330

    Optimize main page with useEffect and useCallback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {useEffect} from "react";
+import {useCallback, useEffect} from "react";
 import {useTelegram} from "@/providers/telegram-provider";
 import {useAppContext} from "@/providers/context-provider";
 import StoreFront from "@/components/store-front";
@@ -10,75 +10,75 @@ export default function Home() {
     const {webApp, user} = useTelegram()
     const {state, dispatch} = useAppContext()
 
+    const mainButtonCallback = useCallback(async () => {
+        if (state.mode === "order") {
+            console.log("checkout!")
+            webApp?.MainButton.showProgress()
+            const invoiceSupported = webApp?.isVersionAtLeast('6.1');
+            const items = Array.from(state.cart.values()).map((item) => ({
+                id: item.product.id,
+                count: item.count
+            }))
+            const body = JSON.stringify({
+                userId: user?.id,
+                chatId: webApp?.initDataUnsafe.chat?.id,
+                invoiceSupported,
+                comment: state.comment,
+                shippingZone: state.shippingZone,
+                items
+            })
+            const res = await fetch("api/orders", {method: "POST", body})
+            const result = await res.json()
+
+            if (invoiceSupported) {
+                webApp?.openInvoice(result.invoice_link, function (status) {
+                    webApp?.MainButton.hideProgress()
+                    if (status == 'paid') {
+                        console.log("[paid] InvoiceStatus " + result);
+                        webApp?.close();
+                    } else if (status == 'failed') {
+                        console.log("[failed] InvoiceStatus " + result);
+                        webApp?.HapticFeedback.notificationOccurred('error');
+                    } else {
+                        console.log("[unknown] InvoiceStatus" + result);
+                        webApp?.HapticFeedback.notificationOccurred('warning');
+                    }
+                });
+            } else {
+                webApp?.showAlert("Some feature not available please update your telegram app!")
+            }
+        } else dispatch({type: "order"})
+    }, [webApp, state.mode, state.cart, state.comment, state.shippingZone])
+
     useEffect(() => {
-        webApp?.BackButton.onClick(() => {
-            dispatch({type: "storefront"})
-        })
+        webApp?.MainButton.setParams({
+            text_color: '#fff',
+            color: '#31b545'
+        }).onClick(mainButtonCallback)
+        webApp?.BackButton.onClick(() => dispatch({type: "storefront"}))
+    }, [webApp, mainButtonCallback])
+
+    useEffect(() => {
         if (state.mode === "storefront")
             webApp?.BackButton.hide()
         else
             webApp?.BackButton.show()
 
-        if (state.mode === "order") {
-            webApp?.MainButton.setParams({
-                text: "CHECKOUT",
-            }).onClick(() => {
-                const invoiceSupported = webApp?.isVersionAtLeast('6.1');
-                if (invoiceSupported) {
-                    console.log("invoice supported");
-                    webApp?.MainButton.showProgress()
+        if (state.mode === "order")
+            webApp?.MainButton.setText("CHECKOUT")
+        else
+            webApp?.MainButton.setText("VIEW ORDER")
+    }, [state.mode])
 
-                    const items = Array.from(state.cart.values()).map((item) => {
-                        return {
-                            id: item.product.id,
-                            count: item.count
-                        }
-                    })
-                    const body = JSON.stringify({
-                        userId: user?.id,
-                        chatId: webApp?.initDataUnsafe.chat?.id,
-                        comment: state.comment,
-                        shippingZone: state.shippingZone,
-                        items
-                    })
-
-                    fetch("api/orders", {method: "POST", body}).then((res) =>
-                        res.json().then((result) => {
-                            webApp?.openInvoice(result.invoice_link, function(status) {
-                                webApp?.MainButton.hideProgress()
-                                if (status == 'paid') {
-                                    console.log("[paid] InvoiceStatus " + result);
-                                    webApp?.close();
-                                } else if (status == 'failed') {
-                                    console.log("[failed] InvoiceStatus " + result);
-                                    webApp?.HapticFeedback.notificationOccurred('error');
-                                } else {
-                                    console.log("[unknown] InvoiceStatus" + result);
-                                    webApp?.HapticFeedback.notificationOccurred('warning');
-                                }
-                            });
-                        })
-                    );
-                } else {
-                    console.log("invoice not supported")
-                }
-            });
-
-        } else if (state.cart.size !== 0) {
-            webApp?.MainButton.setParams({
-                text: "VIEW ORDER",
-                text_color: '#fff',
-                is_visible: true,
-                color: '#31b545'
-            }).onClick(() => {
-                dispatch({type: "order"})
-            })
+    useEffect(() => {
+        if (state.cart.size !== 0) {
+            webApp?.MainButton.show()
             webApp?.enableClosingConfirmation()
         } else {
             webApp?.MainButton.hide()
             webApp?.disableClosingConfirmation()
         }
-    }, [state.mode, state.cart.size, state.cart, state.comment])
+    }, [state.cart.size])
 
     return (
         <main className={`${state.mode}-mode`}>
